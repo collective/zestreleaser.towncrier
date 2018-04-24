a81c404e5bc11d282d4e64e605428929b959698b
Maurits van Rees <maurits@vanrees.org>
2018-04-25 00:42:36 +0200

    Use distutils.spawn.find_executable to find towncrier.

--- src/zestreleaser/towncrier/__init__.py
+++ src/zestreleaser/towncrier/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from zest.releaser import utils
 
+import distutils.spawn
 import logging
 import os
 import sys
@@ -10,6 +11,20 @@ import toml
 logger = logging.getLogger(__name__)
 TOWNCRIER_MARKER = '_towncrier_applicable'
 TOWNCRIER_CONFIG_FILE = 'pyproject.toml'
+
+
+def _towncrier_executable():
+    # Find the towncrier executable.
+    # First option is taken from
+    # https://stackoverflow.com/questions/377017
+    path = distutils.spawn.find_executable('towncrier')
+    if path:
+        return path
+    releaser_dir = os.path.split(os.path.abspath(sys.argv[0]))[0]
+    path = os.path.join(releaser_dir, 'towncrier')
+    if os.path.isfile(path):
+        return path
+    return
 
 
 def _load_config():
@@ -32,8 +47,8 @@ def _is_towncrier_enabled():
     # First check if the towncrier tool is available.
     # towncrier might be on the PATH but not importable,
     # or the other way around.
-    # So could be tricky to know for sure that it is available.
-    # Maybe call 'towncrier --help' and see if that gives an error.
+    if not _towncrier_executable():
+        return False
     # Read pyproject.toml with the toml package.
     if not _load_config():
         return False
@@ -69,21 +84,14 @@ def call_towncrier(data):
     """Entrypoint: run towncrier when available and configured."""
     if not check_towncrier(data):
         return
-    cmd = ['towncrier', '--version', data['new_version'], '--yes']
+    path = _towncrier_executable()
+    cmd = [path, '--version', data['new_version'], '--yes']
     # We would like to pass ['--package' 'package name'] as well,
     # but that is not yet in a release of towncrier.
     logger.info(
         'Running command to update news: %s', utils.format_command(cmd))
-    try:
-        print(utils.execute_command(cmd))
-    except OSError:
-        logger.error('Command failed.  Maybe towncrier is not found.')
-        releaser_dir = os.path.split(os.path.abspath(sys.argv[0]))[0]
-        towncrier_path = os.path.join(releaser_dir, 'towncrier')
-        cmd = [towncrier_path, '--version', data['new_version'], '--yes']
-        logger.info(
-            'Running command to update news: %s', utils.format_command(cmd))
-        print(utils.execute_command(cmd))
+    print(utils.execute_command(cmd))
+
     # towncrier stages the changes with git,
     # which BTW means that our plugin requires git.
     logger.info('The staged git changes are:')
