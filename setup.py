07adb18402351d0949e9ac4c99f6eb7fc935e610
Maurits van Rees <maurits@vanrees.org>
2022-04-19 10:26:48 +0200

    Bumped version for feature release.
    [ci skip]

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='zestreleaser.towncrier',
-    version='1.2.1.dev0',
+    version='1.3.0.dev0',
     description="zest.releaser plugin to call towncrier",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
